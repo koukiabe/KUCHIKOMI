f1fb44a0acbb3ae645bb9e4f650210ecdbd32923
kouki.abe <giants.abechin@gmail.com>
2018-07-22 19:22:23 +0000

    user registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = '登録情報を更新しました'
+      redirect_to @user
+    else
+      flash.now[:danger] = '登録情報の更新に失敗しました'
+      render :edit
+    end
+  end
+  
+end
+
+
+private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homepages#index'
 
+  get 'signup', to: 'users#new'
+  resources :users, only: [:show, :new, :edit, :create, :update]
 end
