e348360e168b0bf87ddd0ec238dee719c93288e7
kouki.abe <giants.abechin@gmail.com>
2018-07-23 15:15:35 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'homepages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :edit, :create, :update]
